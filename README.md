c6a1c8886bfcd5daa7807b300a4783c00a7bb1f9
Viktor Aksyutin <v.aksyutin@spacevm.org>
2024-06-12 21:47:30 +0300

    minor improvements

--- README.md
+++ README.md
@@ -22,7 +22,7 @@
 3. Перейти в каталог со скриптом
 4. Накатить зависимости. (в терминале выполнить команду `pip install -r requirements.txt`)
 5. Получить токен / токены. (пример получения https://t.me/CryptoAutoFarm/5023)
-6. Записать в `config.py`
+6. Записать в `config.py` (рекомендую сделать копию `config_local.py` и писать настройки в него)
 7. Для запуска в терминале выполнить команду: `python3 bot.py`
 
 ![image](https://github.com/TotalAwesome/HamsterKombatFarmer/assets/39047158/6a0eab71-b6b6-4e3b-a5b6-ad1d8ebd85d3)
--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 """
     FEATURES:
-        1. buy_upgrades -> отключение покупки карточек
+        1. buy_upgrades -> управление покупкой карточек. 
+            True -> Включено, 
+            False -> Выключено
         2. buy_decision_method -> метод покупки карточек (
             - price -> покупать самую дешевую
             - payback -> покупать ту, что быстрей всего окупится
@@ -15,16 +17,19 @@
         proxies -> настройки прокси, "кто знает - тот поймет". Если не нужен прокси, лучше убрать
 """
 
+try:
+    from config_local import FEATURES, TOKENS
+except:
 
-FEATURES = {
-    "buy_upgrades": True,
-    "buy_decision_method": "payback",
-    "delay_between_attempts": 60 * 10,
-}
+    FEATURES = {
+        "buy_upgrades": True,
+        "buy_decision_method": "payback",
+        "delay_between_attempts": 60 * 10,
+    }
 
 
-TOKENS = [
-    {"name": "account1", "token": "1234567890MySeCReTt0KeN1", "proxies": {"http": "http://user:pass@10.10.10.0:8080", "https": "https://user:pass@10.10.10.0:9090"}},
-    {"name": "account2", "token": "1234567890MySeCReTt0KeN2", "proxies": {"http": "http://user:pass@10.10.11.0:8080", "https": "https://user:pass@10.10.11.0:9090"}},
-    {"name": "account3", "token": "1234567890MySeCReTt0KeN3", },
-]
+    TOKENS = [
+        {"name": "account1", "token": "1234567890MySeCReTt0KeN1", "proxies": {"http": "http://user:pass@10.10.10.0:8080", "https": "https://user:pass@10.10.10.0:9090"}},
+        {"name": "account2", "token": "1234567890MySeCReTt0KeN2", "proxies": {"http": "http://user:pass@10.10.11.0:8080", "https": "https://user:pass@10.10.11.0:9090"}},
+        {"name": "account3", "token": "1234567890MySeCReTt0KeN3", },
+    ]
